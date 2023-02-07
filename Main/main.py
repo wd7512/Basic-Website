6805c1dfabf4adb6f530d824a8d1f9ec8bc309ad
William Dennis <wwdennis.home@gmail.com>
2023-02-07 21:42:55 +0000

    Updating Web

--- Main/main.py
+++ Main/main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return 'Hello'
+def index():
+    return render_template('index.html')
 
 @app.route("/timetable")
 def mytimetable():
     return '<iframe src="https://calendar.google.com/calendar/embed?src=kcl7cbivblgokbsuo57i8e6k809882bp%40import.calendar.google.com&ctz=Europe%2FLondon" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>'
+
+if __name__ == "__main__":
+    app.run()
